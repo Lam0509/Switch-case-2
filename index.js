384cc2ea46ede018753857cd82c50d6d101a6e1b
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-07 14:39:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function count() {
+    let a = parseInt(document.getElementById("days").value);
+    switch (a) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById("demo").innerHTML = "Tháng " + a + " có 31 ngày";
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            document.getElementById("demo").innerHTML = "Tháng " + a + " có 30 ngày";
+            break;
+        case 2:
+            document.getElementById("demo").innerHTML = "Tháng " + a + " có 28 hoặc 29 ngày"
+    }
+}
